5eafc73c18bee5d97bea5de765a746581a353e19
chandrasekar <chandruselv@gmail.com>
2020-12-31 19:44:58 +0530

    My first app, first commit

--- User_login/settings.py
+++ User_login/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gzq9yu9cl$n4i6yp_n7(ia++_psskq_19upl=^&o%lm@oeo6ze'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
